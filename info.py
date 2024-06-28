056959d4b8fa32d129e7ad0c2fe22f44007d5053
lWhiitel <ed.navarro@duocuc.cl>
2024-06-28 16:44:28 +0000

    Trabajo Eduardo ET DevNet

---
+++ info.py
@@ -0,0 +1,8 @@
+nombre = input("¿Cual es su nombre?: ")
+apellido = input("¿Cual es su apellido?: ")
+edad = input("¿Cual es su edad?: ")
+sede = input("¿Cual es su sede?: ")
+
+print("===============================")
+print("Nombre:  {}\nApellido: {}\nEdad: {}\nSede: {}".format(nombre, apellido, edad, sede ))
+print("===============================")
